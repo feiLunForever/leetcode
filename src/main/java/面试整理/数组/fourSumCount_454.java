297bbd1bde432fc8c9a48e578a9b6d3ec5048fb6
jiangbolun <mengdingbuhui@163.com>
2021-02-12 12:18:58 +0800

    leetcode 454

---
+++ src/main/java/面试整理/数组/fourSumCount_454.java
@@ -0,0 +1,22 @@
+package 面试整理.数组;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class fourSumCount_454 {
+    public static int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                map.put(A[i] + B[j], map.getOrDefault(A[i] + B[j], 0) + 1);
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < C.length; i++) {
+            for (int j = 0; j < D.length; j++) {
+                count += map.getOrDefault(0 - C[i] - D[j], 0);
+            }
+        }
+        return count;
+    }
+}
