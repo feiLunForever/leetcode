b2a62b2b84dc74fe8daaa8f7c5031b7d648385d8
jiangbolun <mengdingbuhui@163.com>
2021-02-14 14:20:23 +0800

    leetcode 206

---
+++ src/main/java/面试整理/链表/reverseList_206.java
@@ -0,0 +1,28 @@
+package 面试整理.链表;
+
+import 初级.链表.reverseList;
+
+public class reverseList_206 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        if (head == null)
+            return null;
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
