3439cdfa2837c6446f469c5381a3d55c3da85204
jiangbolun <mengdingbuhui@163.com>
2021-02-15 21:40:52 +0800

    leetcode 102

---
+++ src/main/java/面试整理/栈和队列/levelOrder_102.java
@@ -0,0 +1,31 @@
+package 面试整理.栈和队列;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class levelOrder_102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                size--;
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
