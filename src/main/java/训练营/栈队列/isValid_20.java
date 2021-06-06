cc1902503222b9a3bf8a58ed95b1dbe389f6391b
jiangbolun <mengdingbuhui@163.com>
2021-06-06 21:52:49 +0800

    leetcode 11

---
+++ src/main/java/训练营/栈队列/isValid_20.java
@@ -0,0 +1,22 @@
+package 训练营.栈队列;
+
+import java.util.Stack;
+
+public class isValid_20 {
+    public boolean isValid(String s) {
+        if ((s.length() & 1) == 1)
+            return false;
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
