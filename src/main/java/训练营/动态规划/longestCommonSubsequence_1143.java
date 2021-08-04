39cb67e564cbcc0e2b141273450f3316767ca123
jiangbolun <mengdingbuhui@163.com>
2021-08-04 23:42:50 +0800

    add

---
+++ src/main/java/训练营/动态规划/longestCommonSubsequence_1143.java
@@ -0,0 +1,18 @@
+package 训练营.动态规划;
+
+public class longestCommonSubsequence_1143 {
+    public static int longestCommonSubsequence(String text1, String text2) {
+        int x = text1.length(), y = text2.length();
+        int[][] dp = new int[x + 1][y + 1];
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= y; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[x][y];
+    }
+}
