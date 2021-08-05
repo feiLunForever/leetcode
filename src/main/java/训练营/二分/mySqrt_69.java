63b418066a3a3d036bcdfbd96a586d6432ce052c
jiangbolun <mengdingbuhui@163.com>
2021-08-05 12:17:33 +0800

    add

--- src/main/java/训练营/二分/mySqrt_69.java
+++ src/main/java/训练营/二分/mySqrt_69.java
@@ -5,19 +5,23 @@ public class mySqrt_69 {
         if (x == 0 || x == 1) {
             return x;
         }
-        int l = 0, r = (x + 1) / 2;
-        while (l <= r) {
-            int mid = l + ((r - l) / 2);
-            int temp = x / mid;
+        int l = 0, r = x / 2;
+        while (l <= r) { // 在区间 [left..right] 查找目标元素
+            int mid = l + (r - l + 1) / 2;
+            int temp = x / mid; // 注意：这里为了避免乘法溢出，改用除法
             if (temp == mid) {
                 return mid;
-            } else if (temp > mid) {
+            } else if (temp > mid) { // 下一轮搜索区间是 [mid+1..right]
                 l = mid + 1;
             } else {
-                r = mid - 1;
+                r = mid - 1; // 下一轮搜索区间是 [left..mid-1]
             }
         }
         return r; // 向下取整，所以返回右边界
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mySqrt(2147483647));
     }
 
 }
