60993d5a9c5da919747f5c9263d1d80886398b4d
jiangbolun <jiangbl1@yonghui.cn>
2022-08-06 22:13:48 +0800

    P21

---
+++ src/main/java/labuladong/leetcode/editor/cn/P21_MergeTwoSortedLists .java
@@ -0,0 +1,99 @@
+//////////////将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+//////////////
+////////////// 
+//////////////
+////////////// 示例 1： 
+//////////////
+////////////// 
+//////////////输入：l1 = [1,2,4], l2 = [1,3,4]
+//////////////输出：[1,1,2,3,4,4]
+////////////// 
+//////////////
+////////////// 示例 2： 
+//////////////
+////////////// 
+//////////////输入：l1 = [], l2 = []
+//////////////输出：[]
+////////////// 
+//////////////
+////////////// 示例 3： 
+//////////////
+////////////// 
+//////////////输入：l1 = [], l2 = [0]
+//////////////输出：[0]
+////////////// 
+//////////////
+////////////// 
+//////////////
+////////////// 提示： 
+//////////////
+////////////// 
+////////////// 两个链表的节点数目范围是 [0, 50] 
+////////////// -100 <= Node.val <= 100 
+////////////// l1 和 l2 均按 非递减顺序 排列 
+////////////// 
+////////////// Related Topics递归 | 链表 
+//////////////
+////////////// 👍 2580, 👎 0 
+//////////////
+//////////////
+//////////////
+//////////////
+////////////
+//////////
+////////
+
+package labuladong.leetcode.editor.cn;
+
+import labuladong.ListNode;
+
+/**
+ * 合并两个有序链表
+ *
+ * @author DY
+ * @date 2022-08-06 16:53:20
+ */
+public class P21_MergeTwoSortedLists {
+    public static void main(String[] args) {
+        //测试代码
+        Solution solution = new P21_MergeTwoSortedLists().new Solution();
+    }
+
+//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            ListNode listNode = new ListNode(-1);
+            ListNode p1 = list1, p2 = list2, p = listNode;
+            while (p1 != null && p2 != null) {
+                if (p1.val < p2.val) {
+                    p.next = p1;
+					p1 = p1.next;
+				} else {
+					p.next = p2;
+					p2 = p2.next;
+				}
+                p = p.next;
+            }
+            if (p1 != null) {
+                p.next = p1;
+            } else if (p2 != null) {
+                p.next = p2;
+            }
+            return listNode.next;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
