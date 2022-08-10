bd282353f833d1f7b300fdb2421732b4c7bdea4c
jiangbolun <jiangbl1@yonghui.cn>
2022-08-10 12:54:47 +0800

    P78

---
+++ src/main/java/labuladong/leetcode/editor/cn/P78_Subsets.java
@@ -0,0 +1,81 @@
+//<p>Given an integer array <code>nums</code> of <strong>unique</strong> elements, return <em>all possible subsets (the power set)</em>.</p>
+//
+//<p>The solution set <strong>must not</strong> contain duplicate subsets. Return the solution in <strong>any order</strong>.</p>
+//
+//<p>&nbsp;</p>
+//<p><strong>Example 1:</strong></p>
+//
+//<pre>
+//<strong>Input:</strong> nums = [1,2,3]
+//<strong>Output:</strong> [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+//</pre>
+//
+//<p><strong>Example 2:</strong></p>
+//
+//<pre>
+//<strong>Input:</strong> nums = [0]
+//<strong>Output:</strong> [[],[0]]
+//</pre>
+//
+//<p>&nbsp;</p>
+//<p><strong>Constraints:</strong></p>
+//
+//<ul>
+//	<li><code>1 &lt;= nums.length &lt;= 10</code></li>
+//	<li><code>-10 &lt;= nums[i] &lt;= 10</code></li>
+//	<li>All the numbers of&nbsp;<code>nums</code> are <strong>unique</strong>.</li>
+//</ul>
+//<details><summary><strong>Related Topics</strong></summary>位运算 | 数组 | 回溯</details><br>
+//
+//<div>👍 1740, 👎 0</div>
+//
+//
+//
+//
+
+package labuladong.leetcode.editor.cn;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Subsets
+ *
+ * @author DY
+ * @date 2022-08-10 12:47:45
+ */
+public class P78_Subsets {
+    public static void main(String[] args) {
+        //测试代码
+        Solution solution = new P78_Subsets().new Solution();
+    }
+
+    //力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> subsets(int[] nums) {
+            LinkedList<Integer> queue = new LinkedList<>();
+            List<List<Integer>> res = new LinkedList<>();
+
+            backTrace(nums, 0, queue, res);
+
+            return res;
+        }
+
+        private void backTrace(int[] nums, int start, LinkedList<Integer> queue, List<List<Integer>> res) {
+
+            res.add(new LinkedList<>(queue));
+
+            for (int i = start; i < nums.length; i++) {
+                queue.add(nums[i]);
+
+                backTrace(nums, i + 1, queue, res);
+
+                queue.removeLast();
+            }
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
