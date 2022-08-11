b9bc2e1f8012349b75ebdc9cfdeafdf062f0567c
jiangbolun <jiangbl1@yonghui.cn>
2022-08-11 17:14:20 +0800

    P40

---
+++ src/main/java/labuladong/leetcode/editor/cn/P40_CombinationSumIi.java
@@ -0,0 +1,95 @@
+//<p>Given a collection of candidate numbers (<code>candidates</code>) and a target number (<code>target</code>), find all unique combinations in <code>candidates</code>&nbsp;where the candidate numbers sum to <code>target</code>.</p>
+//
+//<p>Each number in <code>candidates</code>&nbsp;may only be used <strong>once</strong> in the combination.</p>
+//
+//<p><strong>Note:</strong>&nbsp;The solution set must not contain duplicate combinations.</p>
+//
+//<p>&nbsp;</p>
+//<p><strong>Example 1:</strong></p>
+//
+//<pre>
+//<strong>Input:</strong> candidates = [10,1,2,7,6,1,5], target = 8
+//<strong>Output:</strong> 
+//[
+//[1,1,6],
+//[1,2,5],
+//[1,7],
+//[2,6]
+//]
+//</pre>
+//
+//<p><strong>Example 2:</strong></p>
+//
+//<pre>
+//<strong>Input:</strong> candidates = [2,5,2,1,2], target = 5
+//<strong>Output:</strong> 
+//[
+//[1,2,2],
+//[5]
+//]
+//</pre>
+//
+//<p>&nbsp;</p>
+//<p><strong>Constraints:</strong></p>
+//
+//<ul>
+//	<li><code>1 &lt;=&nbsp;candidates.length &lt;= 100</code></li>
+//	<li><code>1 &lt;=&nbsp;candidates[i] &lt;= 50</code></li>
+//	<li><code>1 &lt;= target &lt;= 30</code></li>
+//</ul>
+//<details><summary><strong>Related Topics</strong></summary>数组 | 回溯</details><br>
+//
+//<div>👍 1066, 👎 0</div>
+//
+//
+//
+//
+
+package labuladong.leetcode.editor.cn;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Combination Sum II
+ *
+ * @author DY
+ * @date 2022-08-11 17:06:19
+ */
+public class P40_CombinationSumIi {
+    public static void main(String[] args) {
+        //测试代码
+        Solution solution = new P40_CombinationSumIi().new Solution();
+    }
+
+    //力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+            List<List<Integer>> res = new LinkedList<>();
+            LinkedList<Integer> queue = new LinkedList<>();
+            Arrays.sort(candidates);
+            backTrack(candidates, 0, target, queue, res);
+            return res;
+        }
+
+        private void backTrack(int[] candidates, int start, int target, LinkedList<Integer> queue, List<List<Integer>> res) {
+            if (target == 0) {
+                res.add(new LinkedList<>(queue));
+                return;
+            }
+            if (target < 0) return;
+            for (int i = start; i < candidates.length; i++) {
+                if (i > start && candidates[i] == candidates[i - 1]) {
+                    continue;
+                }
+                queue.add(candidates[i]);
+                backTrack(candidates, i + 1, target - candidates[i], queue, res);
+                queue.removeLast();
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/main/java/labuladong/leetcode/editor/cn/doc/content/P40_CombinationSumIi.md
@@ -0,0 +1,54 @@
+<p>Given a collection of candidate numbers (<code>candidates</code>) and a target number (<code>target</code>), find all unique combinations in <code>candidates</code>&nbsp;where the candidate numbers sum to <code>target</code>.</p>
+
+<p>Each number in <code>candidates</code>&nbsp;may only be used <strong>once</strong> in the combination.</p>
+
+<p><strong>Note:</strong>&nbsp;The solution set must not contain duplicate combinations.</p>
+
+<p>&nbsp;</p>
+<p><strong>Example 1:</strong></p>
+
+<pre>
+<strong>Input:</strong> candidates = [10,1,2,7,6,1,5], target = 8
+<strong>Output:</strong> 
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]
+</pre>
+
+<p><strong>Example 2:</strong></p>
+
+<pre>
+<strong>Input:</strong> candidates = [2,5,2,1,2], target = 5
+<strong>Output:</strong> 
+[
+[1,2,2],
+[5]
+]
+</pre>
+
+<p>&nbsp;</p>
+<p><strong>Constraints:</strong></p>
+
+<ul>
+	<li><code>1 &lt;=&nbsp;candidates.length &lt;= 100</code></li>
+	<li><code>1 &lt;=&nbsp;candidates[i] &lt;= 50</code></li>
+	<li><code>1 &lt;= target &lt;= 30</code></li>
+</ul>
+<details><summary><strong>Related Topics</strong></summary>数组 | 回溯</details><br>
+
+<div>👍 1066, 👎 0</div>
+
+<div id="labuladong"><hr>
+
+**通知：[数据结构精品课 V1.7](https://aep.h5.xeknow.com/s/1XJHEO) 持续更新中；B 站可查看 [核心算法框架系列视频](https://space.bilibili.com/14089380/channel/series)。**
+
+
+
+<p><strong><a href="https://labuladong.github.io/article?qno=40" target="_blank">⭐️labuladong 题解</a></strong></p>
+</div>
+
+
+
