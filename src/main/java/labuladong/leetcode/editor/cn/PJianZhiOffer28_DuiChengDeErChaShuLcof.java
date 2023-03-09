6d814748eed27adf7b7ab03ea4a95b762a9a551c
jiangbolun <jiangbl1@yonghui.cn>
2023-03-09 14:03:15 +0800

    POffer 28

---
+++ src/main/java/labuladong/leetcode/editor/cn/PJianZhiOffer28_DuiChengDeErChaShuLcof.java
@@ -0,0 +1,50 @@
+/**
+ * English description is not available for the problem. Please switch to Chinese.
+ * <details><summary><strong>Related Topics</strong></summary>树 | 深度优先搜索 | 广度优先搜索 | 二叉树</details><br>
+ *
+ * <div>👍 426, 👎 0<span style='float: right;'><span style='color: gray;'><a href='https://github.com/labuladong/fucking-algorithm/discussions/939' target='_blank' style='color: lightgray;text-decoration: underline;'>bug 反馈</a> | <a href='https://labuladong.gitee.io/article/fname.html?fname=jb插件简介' target='_blank' style='color: lightgray;text-decoration: underline;'>使用指南</a> | <a href='https://labuladong.github.io/algo/images/others/%E5%85%A8%E5%AE%B6%E6%A1%B6.jpg' target='_blank' style='color: lightgray;text-decoration: underline;'>更多配套插件</a></span></span></div>
+ */
+
+package labuladong.leetcode.editor.cn;
+
+import org.example.剑指Offer.TreeNode;
+
+/**
+ * 对称的二叉树  LCOF
+ *
+ * @author DY
+ * @date 2023-03-09 12:48:13
+ */
+public class PJianZhiOffer28_DuiChengDeErChaShuLcof {
+    public static void main(String[] args) {
+        //测试代码
+        Solution solution = new PJianZhiOffer28_DuiChengDeErChaShuLcof().new Solution();
+    }
+
+//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public boolean isSymmetric(TreeNode root) {
+            if (root == null) return true;
+            return recur(root.left, root.right);
+        }
+
+        private boolean recur(TreeNode left, TreeNode right) {
+            if (left == null && right == null) return true;
+            if (left == null || right == null || left.val != right.val) return false;
+            return recur(left.left, right.right) && recur(left.right, right.left);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
